f4d071d48ab6b417e5913e1519ba4b7af1898214
Christian Tarta <cjt36@cs.washington.edu>
2024-12-29 17:32:24 -0800

    Skylander struct

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,7 @@
-use std::{fs, io::{self, Read, Seek, Write}, path};
-
 mod skyutils;
 
+use skyutils::Skylander;
+
 fn main() {
-    new_skylander_file(path::Path::new("../Skylanders/test3.sky")).expect("new sky failure");
-    encryption_skylander_file(path::Path::new("../Skylanders/test.sky"),
-                           path::Path::new("../Skylanders/Ignitor.sky"), false)
-        .expect("file failure");
-    calculate_checksums_file(path::Path::new("../Skylanders/test.sky")).expect("file failure");
-    encryption_skylander_file(path::Path::new("../Skylanders/test2.sky"),
-                           path::Path::new("../Skylanders/test.sky"), true)
-        .expect("file failure");
-}
-
-fn new_skylander_file(file_name: &path::Path) -> io::Result<()> {
-    let mut fd= fs::File::create(file_name)?;
-    let data = skyutils::new_skylander(0x13, 0x2805, Some([0x0F, 0xF0, 0x3C, 0x11]));
-    fd.write_all(&data)?;
-
-    Ok(())
-}
-
-fn encryption_skylander_file(dst: &path::Path, src: &path::Path, encrypt: bool) -> io::Result<()> {
-    let mut fd_src = fs::File::open(src)?;
-    let mut fd_dst = fs::File::create(dst)?;
-    let mut data = [0u8; skyutils::NUM_BYTES];
-    fd_src.read_exact(&mut data)?;
-    skyutils::encryption_skylander(&mut data, encrypt);
-    fd_dst.write_all(&data)?;
-
-    Ok(())
-}
-
-fn calculate_checksums_file(path: &path::Path) -> io::Result<()> {
-    let mut fd = fs::File::options().read(true).write(true).open(path)?;
-    let mut data = [0u8; skyutils::NUM_BYTES];
-    fd.read_exact(&mut data)?;
-    skyutils::calculate_checksums(&mut data);
-    fd.seek(io::SeekFrom::Start(0))?;
-    fd.write_all(&data)?;
-
-    Ok(())
+    println!("Hello, World");
 }
--- src/skyutils.rs
+++ src/skyutils.rs
@@ -3,6 +3,9 @@ use aes::Aes128;
 use block_modes::{block_padding::ZeroPadding, BlockMode};
 use block_modes::Ecb;
 use md_5::{Md5, Digest};
+use std::cmp::min;
+use std::{u16, u32};
+use std::{fs::{self, File}, io::{self, Read, Seek, Write}, path::Path};
 
 type Aes128Ecb = Ecb<Aes128, ZeroPadding>;
 
@@ -10,21 +13,24 @@ const BLOCK_SIZE: usize = 16;
 const BLOCKS_PER_SECTOR: usize = 4;
 const NUM_SECTORS: usize = 16;
 
+/// AREA_BOUNDS[i] is the bounds [start, end) of area i
+const AREA_BOUNDS: [(usize, usize); 4] = [(0x80, 0x110), (0x240, 0x2D0), (0x110, 0x160), (0x2D0, 0x320)];
+
 /// The number of bytes that a Skylander figure (Mifare 1K NFC tag) stores
-pub const NUM_BYTES: usize = BLOCK_SIZE * BLOCKS_PER_SECTOR * NUM_SECTORS;
+const NUM_BYTES: usize = BLOCK_SIZE * BLOCKS_PER_SECTOR * NUM_SECTORS;
 const HASH_CONST: &[u8] = &[
     0x20, 0x43, 0x6F, 0x70, 0x79, 0x72, 0x69, 0x67, 0x68, 0x74, 0x20, 0x28, 0x43, 0x29, 0x20, 0x32,
     0x30, 0x31, 0x30, 0x20, 0x41, 0x63, 0x74, 0x69, 0x76, 0x69, 0x73, 0x69, 0x6F, 0x6E, 0x2E, 0x20,
     0x41, 0x6C, 0x6C, 0x20, 0x52, 0x69, 0x67, 0x68, 0x74, 0x73, 0x20, 0x52, 0x65, 0x73, 0x65, 0x72,
     0x76, 0x65, 0x64, 0x2E, 0x20
-    ];
-    const CRC16_CCITT_FALSE: crc::Algorithm<u16> = crc::Algorithm {
-        width: 16,
-        poly: 0x1021,
-        init: 0xFFFF,
-        refin: false,
-        refout: false,
-        xorout: 0,
+];
+const CRC16_CCITT_FALSE: crc::Algorithm<u16> = crc::Algorithm {
+    width: 16,
+    poly: 0x1021,
+    init: 0xFFFF,
+    refin: false,
+    refout: false,
+    xorout: 0,
     check: 0x29B1,
     residue: 0
 };
@@ -47,67 +53,180 @@ fn key_a(bytes: &[u8]) -> u64 {
     crc.checksum(bytes)
 }
 
-/// Generates an empty Skylander based on the character and variant provided
-/// (not guaranteed that character and variant match -- improper matches will lead to "special" tag in-game).
-/// Returns: figure data
-/// - figure will have an nuid that is provided (default is 00 00 00 00).
-/// - figure will be reset, but ownership is not taken automatically when used.
-pub fn new_skylander(character: u16, variant: u16, nuid: Option<[u8; 4]>) -> [u8; NUM_BYTES] {
-    let mut data = [0u8; NUM_BYTES];
-    let uid = match nuid {
-        Some(v) => v,
-        None => [0u8; 4]
-    };
-    let mut bcc = uid[0];
-    for i in &uid[1..] {
-        bcc ^= *i;
-    }
+pub struct Skylander {
+    data: [u8; NUM_BYTES]
+}
 
-    data[0..4].copy_from_slice(&uid);
-    data[4] = bcc;
-
-    // SAK
-    data[5] = 0x81;
+impl Skylander {
     
-    // ATQA
-    data[6..=7].copy_from_slice(&[0x01, 0x0F]);
-    debug_assert!(data[6] == 0x01 && data[7] == 0x0F);
-
-    // Character
-    data[BLOCK_SIZE..=BLOCK_SIZE + 1].copy_from_slice(&character.to_le_bytes());
-
-    // Variant
-    data[BLOCK_SIZE + 0xC..=BLOCK_SIZE + 0xD].copy_from_slice(&variant.to_le_bytes());
-
-    let crc = crc::Crc::<u16>::new(&CRC16_CCITT_FALSE);
-    let checksum = crc.checksum(&data[0..0x1E]);
-    data[0x1E..=0x1F].copy_from_slice(&u16::to_le_bytes(checksum));
-
-    // Sector 0 trailer
-    data[(3 * BLOCK_SIZE)..(3 * BLOCK_SIZE + 10)]
-        .copy_from_slice(&[0x4B, 0x0B, 0x20, 0x10, 0x7C, 0xCB, 0x0F, 0x0F, 0x0F, 0x69]);
-
-    // Sectors 1 through 15 trailers
-    for i in 1..NUM_SECTORS {
-        let mut bytes = [0u8; 5];
-        let curr_block = (i * BLOCKS_PER_SECTOR + (BLOCKS_PER_SECTOR - 1)) * BLOCK_SIZE;
-        bytes[0..4].copy_from_slice(&data[0..4]);
-        bytes[4] = i as u8;
-        let key_a = &u64::to_le_bytes(key_a(&bytes))[0..6];
-        data[curr_block..curr_block + 6].copy_from_slice(key_a);
-        data[curr_block + 6..curr_block + 10].copy_from_slice(&[0x7F, 0x0F, 0x08, 0x69]);
+    /// Generates an empty Skylander based on the character and variant provided
+    /// (not guaranteed that character and variant match -- improper matches will lead to "special" tag in-game).
+    /// Returns: figure data
+    /// - figure will have an nuid that is provided (default is 00 00 00 00).
+    /// - figure will be reset, but ownership is not taken automatically when used.
+    pub fn new(character: u16, variant: u16, nuid: Option<[u8; 4]>) -> Self {
+        let mut data = [0u8; NUM_BYTES];
+        let uid = match nuid {
+            Some(v) => v,
+            None => [0u8; 4]
+        };
+        let mut bcc = uid[0];
+        for i in &uid[1..] {
+            bcc ^= *i;
+        }
+    
+        data[0..4].copy_from_slice(&uid);
+        data[4] = bcc;
+    
+        // SAK
+        data[5] = 0x81;
+        
+        // ATQA
+        data[6..=7].copy_from_slice(&[0x01, 0x0F]);
+        debug_assert!(data[6] == 0x01 && data[7] == 0x0F);
+    
+        // Character
+        data[BLOCK_SIZE..=BLOCK_SIZE + 1].copy_from_slice(&character.to_le_bytes());
+    
+        // Variant
+        data[BLOCK_SIZE + 0xC..=BLOCK_SIZE + 0xD].copy_from_slice(&variant.to_le_bytes());
+    
+        let crc = crc::Crc::<u16>::new(&CRC16_CCITT_FALSE);
+        let checksum = crc.checksum(&data[0..0x1E]);
+        data[0x1E..=0x1F].copy_from_slice(&u16::to_le_bytes(checksum));
+    
+        // Sector 0 trailer
+        data[(3 * BLOCK_SIZE)..(3 * BLOCK_SIZE + 10)]
+            .copy_from_slice(&[0x4B, 0x0B, 0x20, 0x10, 0x7C, 0xCB, 0x0F, 0x0F, 0x0F, 0x69]);
+    
+        // Sectors 1 through 15 trailers
+        for i in 1..NUM_SECTORS {
+            let mut bytes = [0u8; 5];
+            let curr_block = (i * BLOCKS_PER_SECTOR + (BLOCKS_PER_SECTOR - 1)) * BLOCK_SIZE;
+            bytes[0..4].copy_from_slice(&data[0..4]);
+            bytes[4] = i as u8;
+            let key_a = &u64::to_le_bytes(key_a(&bytes))[0..6];
+            data[curr_block..curr_block + 6].copy_from_slice(key_a);
+            data[curr_block + 6..curr_block + 10].copy_from_slice(&[0x7F, 0x0F, 0x08, 0x69]);
+        }
+    
+        // Area counters must be updated s.t. we can modify a new skylander directly
+        data[0x89] = 0x01;
+        data[0x112] = 0x01;
+    
+        Self { data }
     }
 
-    // Area counters must be updated s.t. we can modify a new skylander directly
-    data[0x89] = 0x01;
-    data[0x112] = 0x01;
+    /// Saves the Skylander to a file
+    /// Overwrites any data up to 1KB from seek start
+    fn save_to_file(&self, file: &mut File) -> io::Result<()> {
+        let mut data = self.data.clone();
+        calculate_checksums(&mut data);
+        encryption_skylander(&mut data, true);
+        file.seek(io::SeekFrom::Start(0))?;
+        file.write_all(&data)?;
 
-    // Because we update area counters, we have to make the data consistent
-    // (although this is not technically a brand-new skylander)
-    calculate_checksums(&mut data);
-    encryption_skylander(&mut data, true);
+        Ok(())
+    }
 
-    data
+    /// Saves the Skylander to a file
+    /// Overwrites any existing data
+    pub fn save_to_filename(&self, filename: &str) -> io::Result<()> {
+        self.save_to_filepath(Path::new(filename))
+    }
+
+    /// Saves the Skylander to a file
+    /// Overwrites any existing data
+    pub fn save_to_filepath(&self, path: &Path) -> io::Result<()> {
+        if path.exists() {
+            let tmp_path = match path.parent() {
+                Some(p) => p.join("tmp.sky"),
+                None => return Err(io::Error::other("can't save to root dir"))
+            };
+            let mut tmp = File::create(&tmp_path)?;
+            self.save_to_file(&mut tmp)?;
+
+            fs::rename(&tmp_path, path)
+        } else {
+            let mut file = File::create_new(path)?;
+            self.save_to_file(&mut file)
+        }
+    }
+
+    /// Reads a Skylander from a file
+    /// Does not verify the data integrity of the file
+    pub fn from_filepath(path: &Path) -> io::Result<Self> {
+        let mut data = [0u8; NUM_BYTES];
+        let mut file = File::open(path)?;
+        file.read_exact(&mut data)?;
+
+        encryption_skylander(&mut data, false);
+
+        Ok(Self { data })
+    }
+
+    /// Reads a Skylander from a file
+    /// Does not verify the data integrity of the file
+    pub fn from_filename(filename: &str) -> io::Result<Self> {
+        Self::from_filepath(Path::new(filename))
+    }
+
+    /// Sets gold of the Skylander to a specified value
+    /// Note that in-game, the gold is capped at 65000
+    pub fn set_gold(&mut self, gold: u16) {
+        self.data[AREA_BOUNDS[0].0 + 0x3..= AREA_BOUNDS[0].0 + 0x4].copy_from_slice(&gold.to_le_bytes());
+        self.data[AREA_BOUNDS[1].0 + 0x3..= AREA_BOUNDS[1].0 + 0x4].copy_from_slice(&gold.to_le_bytes());
+    }
+
+    /// Sets gold of Skylander to max
+    pub fn max_gold(&mut self) {
+        self.set_gold(u16::MAX);
+    }
+
+    
+    /// Sets experience points of the Skylander to specified value
+    /// Max experience in Spyro's Adventure is 33000 (level 10)
+    ///                in Giants is 96500 (level 15)
+    ///                in Swap Force and beyond is 197500 (level 20)
+    /// This function will take min(xp, 197500)
+    pub fn set_xp(&mut self, xp: u32) {
+        let xp1 = min(xp, 33000);
+        let xp2 = min(xp - xp1, 63500);
+        let xp3 = min(xp - xp1 - xp2, 101000);
+
+        debug_assert!(xp1 + xp2 + xp3 == xp || xp1 + xp2 + xp3 == 197500);
+
+        let xp1_bytes = (xp1 as u16).to_le_bytes();
+        let xp2_bytes = (xp2 as u16).to_le_bytes();
+        let xp3_bytes = xp3.to_le_bytes();
+        
+        self.data[AREA_BOUNDS[0].0 ..= AREA_BOUNDS[0].0 + 0x1].copy_from_slice(&xp1_bytes);
+        self.data[AREA_BOUNDS[1].0 ..= AREA_BOUNDS[1].0 + 0x1].copy_from_slice(&xp1_bytes);
+
+        self.data[AREA_BOUNDS[2].0 + 0x5 ..= AREA_BOUNDS[2].0 + 0x6].copy_from_slice(&xp2_bytes);
+        self.data[AREA_BOUNDS[3].0 + 0x5 ..= AREA_BOUNDS[3].0 + 0x6].copy_from_slice(&xp2_bytes);
+        
+        self.data[AREA_BOUNDS[2].0 + 0x8 ..= AREA_BOUNDS[2].0 + 0xB].copy_from_slice(&xp3_bytes);
+        self.data[AREA_BOUNDS[3].0 + 0x8 ..= AREA_BOUNDS[3].0 + 0xB].copy_from_slice(&xp3_bytes);
+    }
+
+    /// Sets experience points of skylander to max
+    pub fn max_xp(&mut self) {
+        self.set_xp(u32::MAX);
+    }
+
+    /// Clears all data from the skylander
+    pub fn clear(&mut self) {
+        for i in 1..NUM_SECTORS {
+            let sector_start = i * BLOCKS_PER_SECTOR * BLOCK_SIZE;
+            let sector_trailer = sector_start + (BLOCKS_PER_SECTOR - 1) * BLOCK_SIZE;
+            self.data[sector_start..sector_trailer].copy_from_slice(&[0u8; (BLOCKS_PER_SECTOR - 1) * BLOCK_SIZE]);
+        }
+
+        // Area counters must be updated s.t. we can modify a cleared skylander directly
+        self.data[AREA_BOUNDS[0].0 + 0x9] = 0x01;
+        self.data[AREA_BOUNDS[2].0 + 0x2] = 0x01;
+    }
 }
 
 /// Encrypts or decrypts the 1K byte Skylander data. Does not check the validity of sector 0 data,
@@ -115,7 +234,7 @@ pub fn new_skylander(character: u16, variant: u16, nuid: Option<[u8; 4]>) -> [u8
 /// - if encrypt is true, then the data in Sectors 1 through 15 (excl. sector trailers) will be encrypted,
 ///   decrypted if false
 /// writes in-place
-pub fn encryption_skylander(data: &mut [u8; NUM_BYTES], encrypt: bool) {
+fn encryption_skylander(data: &mut [u8; NUM_BYTES], encrypt: bool) {
     let mut seed = [0u8; 0x56];
     seed[0..0x20].copy_from_slice(&data[0..0x20]);
     seed[0x21..].copy_from_slice(HASH_CONST);
@@ -139,7 +258,7 @@ pub fn encryption_skylander(data: &mut [u8; NUM_BYTES], encrypt: bool) {
 }
 
 /// Calculates all checksums, writes in place to data array
-pub fn calculate_checksums(data: &mut [u8; NUM_BYTES]) {
+fn calculate_checksums(data: &mut [u8; NUM_BYTES]) {
     // Checksum placeholders
     data[0x8E] = 0x05;
     data[0x8F] = 0x00;
@@ -194,4 +313,29 @@ pub fn calculate_checksums(data: &mut [u8; NUM_BYTES]) {
     type_6_seed[0x20..].copy_from_slice(&data[0x300..0x320]);
 
     data[0x2D0..=0x2D1].copy_from_slice(&crc.checksum(&type_6_seed).to_le_bytes()); // area 3
+}
+
+#[test]
+pub fn test_skylander_file_io() {
+    const FILE_1: &str = "./test1.sky";
+    const FILE_2: &str = "./test2.sky";
+
+    let sky1 = Skylander::new(0x0013, 0x2805, Some([0x20, 0x24, 0x49, 0x12]));
+    sky1.save_to_filename(FILE_1).expect("couldn't save file");
+
+    let sky2 = Skylander::from_filename(FILE_1).expect("couldn't read file");
+    sky2.save_to_filename(FILE_2).expect("couldn't save file");
+
+    let mut data_1 = [0u8; NUM_BYTES];
+    let mut data_2 = [0u8; NUM_BYTES];
+    let mut file_1 = File::open(FILE_1).expect("couldn't open file");
+    let mut file_2 = File::open(FILE_2).expect("couldn't open file");
+
+    file_1.read_exact(&mut data_1).expect("couldn't read file");
+    file_2.read_exact(&mut data_2).expect("couldn't read file");
+
+    assert_eq!(&data_1, &data_2);
+
+    fs::remove_file(Path::new(FILE_1)).expect("couldn't delete file");
+    fs::remove_file(Path::new(FILE_2)).expect("couldn't delete file");
 }
